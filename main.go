b1ff22300dd2497794f2922f3565db849a055dd4
ArtemKhairov <artemxairoff@yandex.ru>
2024-12-29 23:01:23 -0800

    Change language, add if condition

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ func main() {
 	fmt.Println("Body mass index calculator")
 	userHeight, userWeight := getUserParams()
 	BMI := calculateBMI(userHeight, userWeight, BMIPow)
-	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", BMI)
+	if BMI < 16 {
+		fmt.Print("Your weigh is to low")
+	}
+	result := fmt.Sprintf("Your body mass index: %.0f", BMI)
 	fmt.Print(result)
 }
 
@@ -21,9 +24,9 @@ func calculateBMI(userHeight, userWeight, BMIPow float64) float64 {
 func getUserParams() (float64, float64) {
 	var userHeight float64
 	var userWeight float64
-	fmt.Print("Введите рост:")
+	fmt.Print("Enter your height:")
 	fmt.Scan(&userHeight)
-	fmt.Print("Введите вес:")
+	fmt.Print("Enter your weight:")
 	fmt.Scan(&userWeight)
 	return userHeight, userWeight
 }
