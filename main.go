127c2ff90886fa9dba48b5a36d3eb1503fe5abc5
ArtemKhairov <artemxairoff@yandex.ru>
2024-12-29 20:47:12 -0800

    add functions

--- main.go
+++ main.go
@@ -7,14 +7,23 @@ import (
 
 func main() {
 	const BMIPow = 2
+	fmt.Println("Body mass index calculator")
+	userHeight, userWeight := getUserParams()
+	BMI := calculateBMI(userHeight, userWeight, BMIPow)
+	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", BMI)
+	fmt.Print(result)
+}
+
+func calculateBMI(userHeight, userWeight, BMIPow float64) float64 {
+	return userWeight / math.Pow(userHeight, BMIPow)
+}
+
+func getUserParams() (float64, float64) {
 	var userHeight float64
 	var userWeight float64
-	fmt.Println("Body mass index calculator")
 	fmt.Print("Введите рост:")
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите вес:")
 	fmt.Scan(&userWeight)
-	BMI := userWeight / math.Pow(userHeight, BMIPow)
-	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", BMI)
-	fmt.Print(result)
+	return userHeight, userWeight
 }
